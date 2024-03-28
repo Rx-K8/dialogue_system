734f68f8e1633fbaf6bdddf3b06a1637cc6255ac
ka10 <keito.0o0.kirby@gmail.com>
2024-03-28 23:48:19 +0900

    対話システムを組み込む

--- backend/ChatBotAPI/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import SystemMessage
-
-
-class SystemMessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SystemMessage
-        fields = "__all__"
